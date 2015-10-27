bd48a7179a59c9330e92ec57133ef19770e2a05f
lianpeixin <740681141@qq.com>
2015-10-27 23:29:17 +0800

    ignore

--- Application/Home/Controller/MailController.class.php
+++ Application/Home/Controller/MailController.class.php
@@ -1,19 +1,12 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
-use Home\Common;
 
 class MailController extends Controller {
 	public function test() {
-		$mail = new Home\Common\phpmailer(); //建立邮件发送类
-		$mail->CharSet = 'UTF-8';
+		$mail = new \Org\Util\PHPMailer(); //建立邮件发送类
 		$address = '740681141@qq.com';
 		$mail->IsSMTP(); // 使用SMTP方式发送
-		$mail->Host = "smtp.163.com"; // 您的企业邮局域名
-		$mail->SMTPAuth = true; // 启用SMTP验证功能
-		$mail->Username = "lianpeixin@163.com"; // 邮局用户名(请填写完整的email地址)
-		$mail->Password = "super7734652lian"; // 邮局密码
-		$mail->From = "lianpeixin@163.com"; //邮件发送者email地址
 		$mail->FromName = "您的名称";
 		$mail->AddAddress($address, ""); //收件人地址，可以替换成任何想要接收邮件的email信箱,格式是AddAddress("收件人email","收件人姓名")
 		//$mail->AddReplyTo("", "");
